6d41c813750b2c120fe33f9027825038bf931546
aidobilas <aivaras.dobilas@gmail.com>
2023-04-21 14:22:41 +0300

    init

--- script.js
+++ script.js
@@ -25,18 +25,18 @@ for(let i = 0; i < tabs.length; i++){
 
 
 
-// // mySlides
-// var slideIndex = 0;
-// carousel();
+// mySlides
+var slideIndex = 0;
+carousel();
 
-// function carousel() {
-//   var i;
-//   var x = document.getElementsByClassName("mySlides");
-//   for (i = 0; i < x.length; i++) {
-//     x[i].style.display = "none";
-//   }
-//   slideIndex++;
-//   if (slideIndex > x.length) {slideIndex = 1}
-//   x[slideIndex-1].style.display = "block";
-//   setTimeout(carousel, 2000); // Change image every 2 seconds
-// }
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1}
+  x[slideIndex-1].style.display = "block";
+  setTimeout(carousel, 2000); // Change image every 2 seconds
+}
