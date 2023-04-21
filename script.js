f7ce926dcaea132b876c1ea1063083af18b1c5d9
aidobilas <aivaras.dobilas@gmail.com>
2023-04-21 17:50:19 +0300

    init

--- script.js
+++ script.js
@@ -22,21 +22,3 @@ for(let i = 0; i < tabs.length; i++){
     evt.currentTarget.classList.add("selected")
     console.log(evt.currentTarget)
 }  
-
-
-
-// mySlides
-var slideIndex = 0;
-carousel();
-
-function carousel() {
-  var i;
-  var slide = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slide.length; i++) {
-    slide[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slide.length) {slideIndex = 1}
-  slide[slideIndex-1].style.display = "block";
-  setTimeout(carousel, 2000); 
-}
