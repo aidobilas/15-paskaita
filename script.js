12ef9daecc2381f6fe49efd1c96059b6ef340ce3
aidobilas <aivaras.dobilas@gmail.com>
2023-04-21 14:00:19 +0300

    restart

--- script.js
+++ script.js
@@ -21,4 +21,22 @@ for(let i = 0; i < tabs.length; i++){
     // prideda selected klase prie paspausto mygtuko
     evt.currentTarget.classList.add("selected")
     console.log(evt.currentTarget)
-} 
+}  
+
+
+
+// // mySlides
+// var slideIndex = 0;
+// carousel();
+
+// function carousel() {
+//   var i;
+//   var x = document.getElementsByClassName("mySlides");
+//   for (i = 0; i < x.length; i++) {
+//     x[i].style.display = "none";
+//   }
+//   slideIndex++;
+//   if (slideIndex > x.length) {slideIndex = 1}
+//   x[slideIndex-1].style.display = "block";
+//   setTimeout(carousel, 2000); // Change image every 2 seconds
+// }
